475ebe6ab34c8910f41799e94c8dd958a2813049
KevinMolotov <klambert@molotov.tv>
2020-09-09 15:31:31 +0200

    Add option to set custom log levels

--- src/transport.ts
+++ src/transport.ts
@@ -1,8 +1,23 @@
-import * as Sentry from '@sentry/node';
+import * as Sentry from "@sentry/node";
 import TransportStream = require("winston-transport");
 
-export interface SentryTransportOptions extends TransportStream.TransportStreamOptions {
+const DEFAULT_LEVELS_MAP: SeverityOptions = {
+  silly: Sentry.Severity.Debug,
+  verbose: Sentry.Severity.Debug,
+  info: Sentry.Severity.Info,
+  debug: Sentry.Severity.Debug,
+  warn: Sentry.Severity.Warning,
+  error: Sentry.Severity.Error,
+};
+
+export interface SentryTransportOptions
+  extends TransportStream.TransportStreamOptions {
   sentry?: Sentry.NodeOptions;
+  levelsMap?: SeverityOptions;
+}
+
+interface SeverityOptions {
+  [key: string]: Sentry.Severity;
 }
 
 class ExtendedError extends Error {
@@ -19,25 +34,19 @@ class ExtendedError extends Error {
 export default class SentryTransport extends TransportStream {
   public silent = false;
 
-  private levelsMap = {
-    silly: Sentry.Severity.Debug,
-    verbose: Sentry.Severity.Debug,
-    info: Sentry.Severity.Info,
-    debug: Sentry.Severity.Debug,
-    warn: Sentry.Severity.Warning,
-    error: Sentry.Severity.Error
-  };
+  private levelsMap = {};
 
   public constructor(opts?: SentryTransportOptions) {
     super(opts);
 
-    this.silent = opts && opts.silent || false;
-    Sentry.init(this.withDefaults(opts && opts.sentry || {}));
+    this.levelsMap = this.setLevelsMap(opts && opts.levelsMap);
+    this.silent = (opts && opts.silent) || false;
+    Sentry.init(this.withDefaults((opts && opts.sentry) || {}));
   }
 
   public log(info: any, callback: () => void) {
     setImmediate(() => {
-      this.emit('logged', info);
+      this.emit("logged", info);
     });
 
     if (this.silent) return callback();
@@ -46,7 +55,7 @@ export default class SentryTransport extends TransportStream {
 
     const sentryLevel = (this.levelsMap as any)[winstonLevel];
 
-    Sentry.configureScope(scope => {
+    Sentry.configureScope((scope) => {
       if (tags !== undefined && this.isObject(tags)) {
         scope.setTags(tags);
       }
@@ -71,7 +80,8 @@ export default class SentryTransport extends TransportStream {
 
     // Capturing Errors / Exceptions
     if (this.shouldLogException(sentryLevel)) {
-      const error = message instanceof Error ? message : new ExtendedError(info);
+      const error =
+        message instanceof Error ? message : new ExtendedError(info);
       Sentry.captureException(error);
 
       return callback();
@@ -84,23 +94,51 @@ export default class SentryTransport extends TransportStream {
 
   end(...args: any[]) {
     Sentry.flush().then(() => {
-      super.end(...args)
-    })
+      super.end(...args);
+    });
   }
 
   public get sentry() {
     return Sentry;
   }
 
+  private setLevelsMap = (options?: SeverityOptions): SeverityOptions => {
+    if (!options) {
+      return DEFAULT_LEVELS_MAP;
+    }
+
+    const customLevelsMap = Object.keys(options).reduce(
+      (acc: { [key: string]: any }, winstonSeverity: string) => {
+        const sentrySeverity: Sentry.Severity = Sentry.Severity.fromString(
+          options[winstonSeverity]
+        );
+        acc[winstonSeverity] = sentrySeverity;
+
+        return acc;
+      },
+      {}
+    );
+
+    return {
+      ...DEFAULT_LEVELS_MAP,
+      ...customLevelsMap,
+    };
+  };
+
   private withDefaults(options: Sentry.NodeOptions) {
     return {
       ...options,
-      dsn: options && options.dsn || process.env.SENTRY_DSN || '',
-      serverName: options && options.serverName || 'winston-transport-sentry-node',
-      environment: options && options.environment || process.env.SENTRY_ENVIRONMENT || process.env.NODE_ENV || 'production',
-      debug: options && options.debug || !!process.env.SENTRY_DEBUG || false,
-      sampleRate: options && options.sampleRate || 1.0,
-      maxBreadcrumbs: options && options.maxBreadcrumbs || 100
+      dsn: (options && options.dsn) || process.env.SENTRY_DSN || "",
+      serverName:
+        (options && options.serverName) || "winston-transport-sentry-node",
+      environment:
+        (options && options.environment) ||
+        process.env.SENTRY_ENVIRONMENT ||
+        process.env.NODE_ENV ||
+        "production",
+      debug: (options && options.debug) || !!process.env.SENTRY_DEBUG || false,
+      sampleRate: (options && options.sampleRate) || 1.0,
+      maxBreadcrumbs: (options && options.maxBreadcrumbs) || 100,
     };
   }
 
@@ -108,12 +146,12 @@ export default class SentryTransport extends TransportStream {
   //   return msg && msg.message ? msg.message : msg;
   // }
 
-  private isObject (obj: any) {
+  private isObject(obj: any) {
     const type = typeof obj;
-    return type === 'function' || type === 'object' && !!obj;
+    return type === "function" || (type === "object" && !!obj);
   }
 
   private shouldLogException(level: Sentry.Severity) {
     return level === Sentry.Severity.Fatal || level === Sentry.Severity.Error;
   }
-};
+}
